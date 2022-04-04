3a5752ac4afefaa8b8441f50135bc358f8292a91
alicia <aliciafdezrov@gmail.com>
2022-04-04 19:56:44 +0200

    Add not found page

---
+++ src/common/components/not-found/not-found.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './not-found.style.scss';
+import {switchRoutes} from "core/router";
+import {Link} from "react-router-dom";
+
+export const NotFound: React.FC = () => {
+    return (
+        <div className={classes.notFoundContainer}>
+            <h1 className={classes.notFoundText}>
+                404
+            </h1>
+            <h2 className={classes.notFoundText}>Page Not Found</h2>
+
+            <div className={classes.backIndex}>
+                <Link aria-label="link to main login page due to not found"
+                      to={switchRoutes.root}>Back to index</Link></div>
+        </div>
+    );
+};
--- src/core/router/router.component.tsx
+++ src/core/router/router.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {HashRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import {PlanetsScene, StarshipsScene} from "scenes";
 import {switchRoutes} from "./routes";
+import {NotFound} from "common/components";
 
 export const RouterComponent = () => {
     return (
@@ -10,6 +11,7 @@ export const RouterComponent = () => {
                 <Route path={switchRoutes.root} element={<Navigate to={switchRoutes.planets}/>}/>
                 <Route path={switchRoutes.planets} element={<PlanetsScene/>}/>
                 <Route path={switchRoutes.starships} element={<StarshipsScene/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </Router>
     );
